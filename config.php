060e6394fa2691a912829956904f4b73f63c963e
bykovnv <93370282+bykovnv@users.noreply.github.com>
2022-02-17 20:38:41 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "database" =>  
+    [
+    "database" => "php-start",
+    "username" => "php-start_usr",
+    "passwword" => "qjFO2vGtitoQ5y33",
+    "connection" => "mysql:host=localhost",
+    "charset" => "utf8"
+    ],
+];
+?>
